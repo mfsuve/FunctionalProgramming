417b5feb569a1e885ac0ddc81b9fa3321c95892a
mfsuve <suvemustafa@gmail.com>
2018-04-28 17:01:49 +0300

    charCountsSubsets function is implemented

--- bonus3/Main.hs
+++ bonus3/Main.hs
@@ -28,3 +28,18 @@ dictWordsByCharCounts m = fromListWith (++) p
 
 wordAnagrams :: String -> Map CharCount [String] -> [String]
 wordAnagrams w m = findWithDefault [] (wordCharCounts w) m
+
+
+-- convert Map into List
+-- split all counts which are >1 into 1s (expand) (('a', 2) -> ('a', 1), ('a', 1))
+-- get all apossible subsets
+-- remove duplicates
+-- convert back all 1s by adding (('a', 1), ('a', 1) -> ('a', 2))
+charCountsSubsets :: CharCount -> [[(Char, Int)]]
+charCountsSubsets cc' = map toList $ map (fromListWith (+)) $ nub $ subsets $ expand $ toList cc'
+  where
+    expand []           = []
+    expand ((c, 1):ccs) = (c, 1):(expand ccs)
+    expand ((c, i):ccs) = (c, 1):(expand ((c, i-1):ccs))
+    subsets []       = [[]]
+    subsets (cc:ccs) = subsets ccs ++ map (cc:) (subsets ccs)
